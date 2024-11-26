16152c29251ced44ae2f24f6da2eb0498abd90f2
Adnitop <adekolaoyeriyi4@gmail.com>
2024-11-26 15:21:26 +0100

    Add files via upload

---
+++ quiz2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+struct QUIZ{
+string Question;
+string Option[4];
+int CorrectAnswer; };
+int main(){
+
+QUIZ questions[]= {{"What is the past tense of GO?",{ "Come", "Went", "Gone", "Came"}, 2},{"What is the comparator for GOOD?", { "Better","Gooder", "Best", "Good"}, 1}, {"What is the opposite of COLD?", {"hot","Warm","Cool"}, 1},{"what is the superator for GREAT?", {"Best", "Super", "Good", "Greatest"}, 4}, {"As SILENT as_____?",{"Graveyard","Club", "Party", "Market"}, 1}};
+
+ int i,j; int score=0; int percentage=0;
+ for(i=0; i<5; i++){
+    cout<<i+1<< questions[i].Question<<endl;
+    for (j=0; j<4; j++){
+        cout << j+1 << questions[i].Option[j]<<endl;
+
+}
+cout<<"CHOOSE THE CORRECT ANSWER:"<<"";
+    int answer;
+
+    cin>>answer;
+    if(answer==questions[i].CorrectAnswer){
+        cout<< "CORRECT!!!!"<< endl;
+        cout<< " "<<endl;
+        score++;
+    }
+    else {
+        cout << "wrong"<<endl;
+        cout<<" ";
+    }
+
+
+ }
+cout<<"YOU SCORED"<<"  "<<score<<endl;
+percentage= score*20;
+cout<<"PERCENTAGE"<<percentage<<"%"<<endl;
+if (percentage>=50){
+    cout<<"Hurray!!!! You passed";
+}
+else {
+    cout<< "You failed. you can do better next time";
+}
+
+}
+
+
+
+
+
